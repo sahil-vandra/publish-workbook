51ed2a773a1c1566d7a74ff88b84bf6ff426bb0f
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-09-16 18:32:09 +0530

    s

--- publish_twb_workbook.py
+++ publish_twb_workbook.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         '--username', help='username to sign into server')
     parser.add_argument(
         '--password', help='filepath to the workbook to publish')
-    parser.add_argument('--filepath',"-u", help='filepath to the workbook to publish', type=str, action="store_true")
+    parser.add_argument('--filepath',"-u", help='filepath to the workbook to publish', action="store_true")
     parser.add_argument('--project_name', action='store',
                         type=str)
 
